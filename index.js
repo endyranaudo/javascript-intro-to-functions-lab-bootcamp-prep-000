ae10007c5153f9e445a126017497574e704fa40a
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-01 15:50:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+// 1
+function shout(string) {
+  return string.toUpperCase()
+}
+ //3//
+function logShout(string){
+  console.log(string.toUpperCase)
+}
+
+// 2
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+// 3
+function logShout(string) {
+  console.log(string.toLowerCase())
+}
+
+// 4
+function logWhisper(string) {
+  
+}
+
+// 5
+function sayHiToGrandma(string){
+  var uppercase = "HELLO"	
+  var lowercase = "hello"	
+
+
+   if (string === lowercase){	  
+     if (string === string.toLowerCase()){
+    return "I can't hear you!"	    return "I can't hear you!"
+  } else if (string === uppercase){	
+    }else if (string === string.toUpperCase()){
+    return "YES INDEED!"	    return "YES INDEED!"
+  }else if (string === "I love you, Grandma."){	  }else if (string === "I love you, Grandma."){
+    return "I love you, too."	    return "I love you, too."
