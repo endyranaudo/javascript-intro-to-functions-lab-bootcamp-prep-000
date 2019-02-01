5a58db1d9c2880d22f3f1e6a19ca04f5cd95c17e
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-01 15:52:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ function shout(string){
 
  logWhisper("HELLO")
 
- function sayHiToGrandma(string){
+ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
   } else if (string === string.toUpperCase()) { 
     return "YES INDEED!";
+ }
 }
 
 sayHiToGrandma("hello");
