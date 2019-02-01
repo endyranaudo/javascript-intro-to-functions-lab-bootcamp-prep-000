bc6b758abc5602602971ac0487be947c9d1c3e79
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-01 15:53:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
+// 1
 function shout(string){
   return string.toUpperCase()
 };
 
 
-
+// 2
 function logShout(string){
   return console.log(string.toUpperCase())
 }
 
 
-
+// 3
 function whisper(string){
   return string.toLowerCase();
 }
 
 
-
+// 4
 function logWhisper(string){
   return console.log(string.toLowerCase())
 }
 
 
-
+// 5
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
